90d6d87e1ae3c826a49aaab514db5872ffae371b
Toby Murfet <toby.murfet@griffithuni.edu.au>
2022-09-09 12:28:20 +1000

    host angular on node server

--- server/server.js
+++ server/server.js
@@ -3,23 +3,21 @@ var app = express();
 
 var http = require('http').Server(app);
 
+var path = require('path');
+
+
 var bodyParser = require("body-parser");  //body-parser
 app.use(bodyParser.json);
 
+var cors = require('cors'); //cross origin resource sharing
+app.use(cors());
+
+
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, '../dist/frontend/')));
 
-app.use(express.static(__dirname + ''));
-
-let server = http.listen(3000, function () {
-    let host = server.address().address;
-    let port = server.address().port;
-    console.log("Server listening on: " + host + " port:" + port); 
+app.listen(port,() => {
+    console.log("listening at port: ", port);
 });
-
-app.get("/", (req, res) => {
-    console.log("Server is running at port", port);
-    res.send("hello world");
-  });
-
 
